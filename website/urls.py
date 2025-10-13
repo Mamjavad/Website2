d71c58c793174ebbcc85b38a9a683022ad3aab5a
Mamjavad <mohammadjavad.km83@gmail.com>
2025-10-13 14:43:54 +0330

    created dynamic url and base footer and header
    i created dynamic ulr fot my site and created base footer and base header

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
+
 urlpatterns = [
-    path('',index_views),
-    path('About',About_views),
-    path('contact',contact_views)
+    path('',index_views,name='index'),
+    path('About',About_views,name='about'),
+    path('contact',contact_views,name='contact')
 
 ]
